958f04a6cde4be54e290a79b1a3b80c8600205a9
aronresni <aronresni@gmail.com>
2023-11-12 22:11:33 -0300

    upload

--- components/about.tsx
+++ components/about.tsx
@@ -17,32 +17,16 @@ export default function About() {
       transition={{ delay: 0.175 }}
       id="about"
     >
-      <SectionHeading>About me</SectionHeading>
+      <SectionHeading>Sobre mi</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
+        Despues de graduarme en {" "}
+        <span className="font-medium">Henry como desarrollador Full-Stack</span>,Decidi comenzar a trabajar como desarrollador freelancer en donde aprendi y pude tomar trabajos para ganar experiencia.{" "}
+        <span className="italic">Mi parte favorita de programar</span> es la resolucion de problemas.
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+         Mi stack principal es React, Next.js, Node.js, and PostgreeSQL. {" "}
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        Estoy algo familiarizado con Typescript. Estoy constantemente aprendiendo nuevos lenguahes. Actualmente estoy buscando{" "}
+        <span className="font-medium">un posicion full-time</span> como desarrollador Trainee/Junior!
       </p>
     </motion.section>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
+import accesbank from "@/public/acces-bank.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
@@ -12,35 +12,35 @@ export const links = [
     hash: "#home",
   },
   {
-    name: "About",
+    name: "Sobre",
     hash: "#about",
   },
   {
-    name: "Projects",
+    name: "Proyectos",
     hash: "#projects",
   },
   {
-    name: "Skills",
+    name: "Habilidades",
     hash: "#skills",
   },
   {
-    name: "Experience",
+    name: "Experiencia",
     hash: "#experience",
   },
   {
-    name: "Contact",
+    name: "Contacto",
     hash: "#contact",
   },
 ] as const;
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Carrera de Desarrollo Frontend React",
+    location: "Argentina",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "Enero",
   },
   {
     title: "Front-End Developer",
@@ -66,7 +66,7 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: accesbank,
   },
   {
     title: "rmtDev",
@@ -94,14 +94,11 @@ export const skillsData = [
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
-  "MongoDB",
+  "Boostrap",
   "Redux",
-  "GraphQL",
-  "Apollo",
   "Express",
+  "Sequelize",
   "PostgreSQL",
-  "Python",
-  "Django",
+  "Figma",
   "Framer Motion",
 ] as const;
